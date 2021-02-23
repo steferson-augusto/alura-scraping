f409d7c2572372726c720e6178d3018cf708baca
Steferson Augusto <steferson_a@hotmail.com>
2021-02-22 23:43:19 -0400

    feat: cria função para realizar login

---
+++ src/services/login.ts
@@ -0,0 +1,23 @@
+import ora from 'ora'
+import { Page } from 'puppeteer'
+require('dotenv').config()
+
+export default async function login(page: Page, prefix: string = '') {
+  const spinner = ora('Realizando login...')
+  spinner.prefixText = prefix
+  spinner.start()
+  try {
+    await page.goto('https://cursos.alura.com.br/loginForm')
+    
+    await page.type('[name="username"]', process.env.EMAIL as string)
+    await page.type('[name="password"]', process.env.PASSWORD as string)
+  
+    await page.click('.btn-login')
+  
+    await page.waitForSelector('.profile-info-name')
+    
+    spinner.succeed('Login realizado com sucesso')
+  } catch (error) {
+    spinner.fail('Falha ao fazer login')
+  }
+}
