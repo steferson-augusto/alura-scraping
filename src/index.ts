30c7b7862f905c6d99e89d224058abf6c5f237ad
Steferson Augusto <steferson_a@hotmail.com>
2021-03-21 19:40:16 -0400

    feat: abstrai funçaõ de login

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
 import puppeteer from 'puppeteer'
-require('dotenv').config()
 
-import getAtividades from './services/getAtividades'
 import message from './utils/message'
+import login from './services/login'
+import getAtividades from './services/getAtividades'
 // import getAulas from './services/getAulas'
 // import getCursos from './services/getCursos'
 // import getFormacoes from './services/getFormacoes'
 
 
 const run = async () => {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(0)
-
-  await page.goto('https://cursos.alura.com.br/loginForm')
-
-  await page.type('[name="username"]', process.env.EMAIL as string)
-  await page.type('[name="password"]', process.env.PASSWORD as string)
-
-  await page.click('.btn-login')
-
-  await page.waitForNavigation()
-
-  // await getFormacoes(page)
-  // await getCursos(page)
-  // await getAulas(page)
+  try {
+    const browser = await puppeteer.launch({ headless: true })
+    let page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0)
   
-  await getAtividades(page)
-  message.success('✔ Extração concluída com sucesso')
-  await browser.close()
+    await login(page)
+  
+    // await getFormacoes(page)
+    // await getCursos(page)
+    // await getAulas(page)
+    
+    // 15, 80, 94, 115. 123, 129, 139, 160, 182, 217, 240, 257, 265, 271, 275, 343
+    await getAtividades(page, 343)
+
+    message.success('✔ Extração concluída com sucesso')
+    await browser.close()
+  } catch (error) {
+    message.error(error)
+  }
 }
 
 run()
